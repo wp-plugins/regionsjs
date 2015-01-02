d05ab64f7082774411e5b7cbb2ff84834561b8b0
Ramoonus <Ramoonus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-02 10:47:27 +0000

    v1.0.1 in trunk
    git-svn-id: https://plugins.svn.wordpress.org/regionsjs/trunk@1058344 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,11 +3,12 @@
 Contributors: ramoonus
 Donate link: http://www.ramoonus.nl
 Tags: jquery, jquery mobile, mobile, javascript
-Requires at least: 3.2.0
-Tested up to: 3.9.1
-Stable tag: 1.0.0
+Requires at least: 3.9
+Tested up to: 4.1
+Stable tag: 1.0.1
 
-A polyfill for the Adobe's CSS3 proposal for Text regions (Tested + Works on Chrome, Safari, Firefox, IE 8+) 
+A polyfill for the Adobe's CSS3 proposal for Text regions (Tested + Works on Chrome, Safari, Firefox, IE 8+)
+
 == Description ==
 A polyfill for the Adobe's CSS3 proposal for Text regions (Tested + Works on Chrome, Safari, Firefox, IE 8+) 
 
@@ -25,5 +26,8 @@ Its recommended to flush the cache after upgrading.
 Not relevant.
 
 == Changelog ==
+= 1.0.1 =
+* Updated
+
 = 1.0.0 =
 * Based on Region.js 17-6-2001
--- regionsjs.php
+++ regionsjs.php
@@ -1,16 +1,15 @@
 <?php
 /*
-Plugin Name: Regions.js
+Plugin Name: CSS Regions polyfill
 Plugin URI: http://www.ramoonus.nl/wordpress/regionsjs/
 Description: A polyfill for the Adobe's CSS3 proposal for Text regions (Tested + Works on Chrome, Safari, Firefox, IE 8+) 
-Version: 1.0.0
+Version: 1.0.1
 Author: Ramoonus
 Author URI: http://www.ramoonus.nl/
 */
 function rw_regionjs() {
-		wp_deregister_script('region'); // deregister
-		wp_register_script('region', plugins_url('/js/regions.jquery.min.js', __FILE__), array("jquery", "lettering"), '0.1'); // re register
-		wp_enqueue_script('region'); // load
+		wp_deregister_script('regions'); // deregister
+		wp_enqueue_script('regions', plugins_url('/js/cssregions.min.js', __FILE__), array("jquery"), '1.0.1');
 } 
-add_action('init', 'rw_regionjs');
+add_action('wp_enqueue_scripts', 'rw_regionjs');
 ?>
